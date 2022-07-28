1576b10fe7e34f04d47cca2aeddd1a41ca872786
JustTyping1 <aditya.chorghade@outlook.com>
2022-07-28 19:15:42 +0100

    changes

--- scraper.go
+++ scraper.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"bufio"
+	"os"
 
 	"github.com/gocolly/colly"
 	//"strconv"
 )
 
 func main() {
-	scanner = bufio.NewScanner(os)
-
+	println("ENTER CMD >")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
 	c := colly.NewCollector()
 	c.OnHTML("span[class=wr-value--temperature--c]", func(h *colly.HTMLElement) {
 		println(h.ChildText("span[aria-hidden=true]"))
 	})
-	c.Visit("https://www.bbc.co.uk/weather/2649452")
+	c.Visit("")
 
 }
