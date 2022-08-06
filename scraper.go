dd02ade5a7154495f3f69ff45d9dc7d6ab044ef8
JustTyping1 <aditya.chorghade@outlook.com>
2022-08-06 09:39:01 +0100

    stocks

--- scraper.go
+++ scraper.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gocolly/colly"
 	//"strconv"
 )
 
 func main() {
-	c := colly.NewCollector()
-	c.OnHTML("div[aria-live=polite]", func(h *colly.HTMLElement) {
-		println(h.ChildText("div[data-val]"))
+	var running bool = true
+	for running {
+		println("ENTER CMD >")
+		var command string
+		fmt.Scanln(&command)
+		if command == "w" {
+			c := colly.NewCollector()
+			c.OnHTML("div.main-temp", func(h *colly.HTMLElement) {
+				println(h.ChildText("span.currTemp") + " Degrees Celcius")
 
-	})
-	c.Visit("https://www.bing.com/search?q=weather")
+			})
+			c.Visit("https://uk.search.yahoo.com/search?p=weather")
+		}
+		if command == "s" {
+			a := colly.NewCollector()
+			a.OnHTML("div.QuoteStrip-lastPriceStripContainer", func(h *colly.HTMLElement) {
+				println("DJI :")
+				println(h.ChildText("span.QuoteStrip-lastPrice"))
+				println(h.ChildText("span.QuoteStrip-changeUp"))
+			})
+			a.Visit("https://www.cnbc.com/quotes/.DJI?qsearchterm=Dow%20Jones%20Industrial%20Average")
 
+			b := colly.NewCollector()
+			b.OnHTML("div.QuoteStrip-lastPriceStripContainer", func(h *colly.HTMLElement) {
+				println("DJI :")
+				println(h.ChildText("span.QuoteStrip-lastPrice"))
+				println(h.ChildText("span.QuoteStrip-changeUp"))
+			})
+			b.Visit("https://www.cnbc.com/quotes/.DJI?qsearchterm=Dow%20Jones%20Industrial%20Average")
+		}
+	}
 }
